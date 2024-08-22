2fd54722f896c12708117ee99e6b64e86b2c779d
Lucas Bailo <lucasebailo@gmail.com>
2024-08-22 11:50:18 -0300

    fixing about us page 2

--- src/components/AboutUs/aboutUs.jsx
+++ src/components/AboutUs/aboutUs.jsx
@@ -1,9 +1,9 @@
-import CatImg from '../images/hire-me.jpg'
+// import CatImg from '../images/hire-me.jpg'
 
 export default function AboutUs() {
     return (
         <main className="main-box p-5 main-box-bg">
-            <img src={CatImg} alt="" />
+            {/* <img src={CatImg} alt="" /> */}
             <ul className='flex flex-col items-center justify-center h-full text-xl underline animate-bounce gap-2'>
                 <li><a href="https://github.com/lucasbailo">My GitHub</a></li>
                 <li><a href="https://www.instagram.com/lucassbailo/">Instagram - @lucassbailo</a></li>
