28b5e304946e1b68a0595690a06d546d1e3d521c
Lucas Bailo <lucasebailo@gmail.com>
2024-08-28 18:06:04 -0300

    fixing gr of protein delay

--- src/components/proteinCalc/index.jsx
+++ src/components/proteinCalc/index.jsx
@@ -1,13 +1,14 @@
 // import { useLocation } from 'react-router-dom';
 
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Protein() {
 
     // const {state} = useLocation();
     // const {username, useremail} = state;
 
-    const [dataGoal,setDataGoal]  = useState("Average person");
+    const [dataGoal,setDataGoal]  = useState("");
+    console.log("Fora: " + dataGoal)
     const dataWeight = useRef(0);
     const [result, setResult] = useState(0)
     const [grProtein, setGrProtein] = useState(0)
@@ -18,8 +19,10 @@ export default function Protein() {
     // Weight loss: Recommended intake is 1-1.2 grams per kilogram.
     
     // Here we're goingo to use this function to define the amount of protein in gr for each Kg using the age, and then multiply it for the person's weight
+    
     const defineProteinNeed = () => {
-       if ( dataGoal == "" ) {
+       
+        if ( dataGoal == "" ) {
         return "";
        } else if ( dataGoal === "Average person" || dataGoal == "Person with diabetes" ) {
         return 0.8;
@@ -31,27 +34,37 @@ export default function Protein() {
         return "";
        }
     }
+
+    const mudarTudo = (e) => {
+        setDataGoal(e.target.value)
+        console.log("Dentro: " + dataGoal)
+    }
+    
     
     const doProteinCalculate = () => {
-        
-        defineProteinNeed()
         
         const proteinNeed = defineProteinNeed() // creating the variable with the input
         
         const weight = dataWeight.current.value //colect the person's weight
-
+        
         const totalProteinNeed = (weight ? ((proteinNeed * weight)) : 0) // formula to give the amount of water in liters (ml of water per body kg x person's weight)
-
+        
         // To see if the results and data colected is right
         console.log('Protein need in gr: ' + proteinNeed)
         console.log('Total Protein need: ' + totalProteinNeed)
         console.log('Goal: ' + dataGoal)
         console.log('Weight: ' + dataWeight.current.typeof)
-
+        
         setResult(totalProteinNeed) // changing the state with the current results to display these information.
         setGrProtein(proteinNeed)
     }
 
+    useEffect(() => {
+        doProteinCalculate()  
+    }, [dataGoal]
+    )
+    
+    
     return (
         <main className='main-box flex-col items-center main-box-bg'>
             <div><h1 className='pt-10'>👷‍♂️💻I'M STILL WORKING ON THIS ONE...💻👷‍♂️</h1></div>
@@ -64,27 +77,19 @@ export default function Protein() {
                             </div>
                             <div className='flex gap-3'>
                                 <label htmlFor="average">Average person</label>
-                                <input type="radio" id="average" name="goal" value="Average person" onChange={e => { 
-                                    setDataGoal(e.target.value)
-                                    doProteinCalculate()}} />
+                                <input type="radio" id="average" name="goal" value="Average person" onChange={mudarTudo} />
                             </div>
                             <div className='flex gap-3'>
                                 <label htmlFor="diabetes">Person with diabetes</label>
-                                <input type="radio" id="diabetes" name="goal" value="Person with diabetes" onChange={e => {
-                                    setDataGoal(e.target.value)
-                                    doProteinCalculate()}} /> 
+                                <input type="radio" id="diabetes" name="goal" value="Person with diabetes" onChange={mudarTudo} /> 
                             </div>
                             <div className='flex gap-3'>
                                 <label htmlFor="build">Build muscle</label>
-                                <input type="radio" id="build" name="goal" value="Build muscle" onChange={e => {
-                                    setDataGoal(e.target.value)
-                                    doProteinCalculate()}} />
+                                <input type="radio" id="build" name="goal" value="Build muscle" onChange={mudarTudo} />
                             </div>
                             <div className='flex gap-3'>
                                 <label htmlFor="loss">Weight loss</label>
-                                <input type="radio" id="loss" name="goal" value="Weight loss" onChange={e => {
-                                    setDataGoal(e.target.value)
-                                    doProteinCalculate()}} />
+                                <input type="radio" id="loss" name="goal" value="Weight loss" onChange={mudarTudo} />
                             </div>
                         </div>
                         <div className='mt-5'>
